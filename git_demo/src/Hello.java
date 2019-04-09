765e211be5cbc99b826b6f5f47384b39a5a0a79a
Billy <44896497+BeataWang@users.noreply.github.com>
2019-04-09 20:41:26 +0800

    idea上传到仓库

---
+++ git_demo/src/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * @author : liangqi
+ * @Description : java类作用描述
+ * @CreateDate : 2019/4/9 20:39
+ * @Updat :  更新作者
+ * @UpdateDate : 2019/4/9 20:39
+ * @UpdateRemark : 更新内容注释
+ * @Version : 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
